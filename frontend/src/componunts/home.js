3988089e1e1712d2ccd30e510bc154231c44e15f
saravanan28saro-ux <saravanan28saro@gmail.com>
2026-03-31 16:33:55 +0530

    corrected code

--- frontend/src/componunts/home.js
+++ frontend/src/componunts/home.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+
 import {Container,Li,Card} from "./styledcomponunts";
 import { Link } from "react-router-dom";
 
 export const Home=()=>{
-    const [bloodtypes,setbloodtypes] = useState({
+    const bloodtypes = {
         "A+":10,
         "A-":5,
         "B+":8,
@@ -12,7 +12,7 @@ export const Home=()=>{
         "AB-":2,
         "O+":12,
         "O-":6
-    })
+    }
     
     return(          
             <Container>
